577da6d5811fa1bc376b00787dffb41a38048357
Evehdz <88472990+Evehdz@users.noreply.github.com>
2021-08-05 02:05:41 -0500

    Add files via upload

---
+++ Cartelera/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace Cartelera
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnRyF = new System.Windows.Forms.Button();
+            this.btnRyF2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBW = new System.Windows.Forms.Button();
+            this.btnBW2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSilencio = new System.Windows.Forms.Button();
+            this.btnSilencio2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel4 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(367, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CARTELERA";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Controls.Add(this.btnRyF);
+            this.flowLayoutPanel1.Controls.Add(this.btnRyF2);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(58, 100);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(204, 270);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // btnRyF
+            // 
+            this.btnRyF.BackColor = System.Drawing.Color.Transparent;
+            this.btnRyF.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRyF.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRyF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRyF.Location = new System.Drawing.Point(3, 3);
+            this.btnRyF.Name = "btnRyF";
+            this.btnRyF.Size = new System.Drawing.Size(201, 23);
+            this.btnRyF.TabIndex = 2;
+            this.btnRyF.Text = "Rapidos y Furiosos 9";
+            this.btnRyF.UseVisualStyleBackColor = false;
+            this.btnRyF.Click += new System.EventHandler(this.btnRyF_Click);
+            // 
+            // btnRyF2
+            // 
+            this.btnRyF2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRyF2.BackgroundImage")));
+            this.btnRyF2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRyF2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRyF2.Location = new System.Drawing.Point(3, 32);
+            this.btnRyF2.Name = "btnRyF2";
+            this.btnRyF2.Size = new System.Drawing.Size(201, 225);
+            this.btnRyF2.TabIndex = 2;
+            this.btnRyF2.UseVisualStyleBackColor = true;
+            this.btnRyF2.Click += new System.EventHandler(this.btnRyF2_Click);
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel2.Controls.Add(this.btnBW);
+            this.flowLayoutPanel2.Controls.Add(this.btnBW2);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(327, 100);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(204, 270);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // btnBW
+            // 
+            this.btnBW.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBW.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBW.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBW.Location = new System.Drawing.Point(3, 3);
+            this.btnBW.Name = "btnBW";
+            this.btnBW.Size = new System.Drawing.Size(201, 23);
+            this.btnBW.TabIndex = 0;
+            this.btnBW.Text = "Black Widow";
+            this.btnBW.UseVisualStyleBackColor = true;
+            this.btnBW.Click += new System.EventHandler(this.btnBW_Click);
+            // 
+            // btnBW2
+            // 
+            this.btnBW2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBW2.BackgroundImage")));
+            this.btnBW2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnBW2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnBW2.Location = new System.Drawing.Point(3, 32);
+            this.btnBW2.Name = "btnBW2";
+            this.btnBW2.Size = new System.Drawing.Size(201, 225);
+            this.btnBW2.TabIndex = 0;
+            this.btnBW2.UseVisualStyleBackColor = true;
+            this.btnBW2.Click += new System.EventHandler(this.btnBW2_Click);
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel3.Controls.Add(this.btnSilencio);
+            this.flowLayoutPanel3.Controls.Add(this.btnSilencio2);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(596, 100);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(204, 270);
+            this.flowLayoutPanel3.TabIndex = 3;
+            // 
+            // btnSilencio
+            // 
+            this.btnSilencio.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSilencio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSilencio.Location = new System.Drawing.Point(3, 3);
+            this.btnSilencio.Name = "btnSilencio";
+            this.btnSilencio.Size = new System.Drawing.Size(201, 23);
+            this.btnSilencio.TabIndex = 0;
+            this.btnSilencio.Text = "Un lugar en silencio parte II";
+            this.btnSilencio.UseVisualStyleBackColor = true;
+            this.btnSilencio.Click += new System.EventHandler(this.btnSilencio_Click);
+            // 
+            // btnSilencio2
+            // 
+            this.btnSilencio2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSilencio2.BackgroundImage")));
+            this.btnSilencio2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSilencio2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSilencio2.Location = new System.Drawing.Point(3, 32);
+            this.btnSilencio2.Name = "btnSilencio2";
+            this.btnSilencio2.Size = new System.Drawing.Size(201, 225);
+            this.btnSilencio2.TabIndex = 0;
+            this.btnSilencio2.UseVisualStyleBackColor = true;
+            this.btnSilencio2.Click += new System.EventHandler(this.btnSilencio2_Click);
+            // 
+            // flowLayoutPanel4
+            // 
+            this.flowLayoutPanel4.BackColor = System.Drawing.Color.Black;
+            this.flowLayoutPanel4.Location = new System.Drawing.Point(0, 38);
+            this.flowLayoutPanel4.Name = "flowLayoutPanel4";
+            this.flowLayoutPanel4.Size = new System.Drawing.Size(846, 26);
+            this.flowLayoutPanel4.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel4);
+            this.Controls.Add(this.flowLayoutPanel3);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnRyF;
+        private System.Windows.Forms.Button btnRyF2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button btnBW;
+        private System.Windows.Forms.Button btnBW2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button btnSilencio;
+        private System.Windows.Forms.Button btnSilencio2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel4;
+    }
+}
+
---
+++ Cartelera/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using HorarioFyF;
+using HorarioBW;
+using HorarioSilencio;
+using System.Windows.Forms;
+
+namespace Cartelera
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * La instrucción que tiene todos los botones en este código es mostrar 
+         * la ventana del horario y sinopsis de la pelicula dependiendo a cual
+         * se le de click
+         */
+        private void btnRyF_Click(object sender, EventArgs e)
+        {
+            FormHorarioFyF formhorariofyf = new FormHorarioFyF(this);
+            formhorariofyf.Show();
+            this.Hide();
+           
+        }
+
+        private void btnRyF2_Click(object sender, EventArgs e)
+        {
+            FormHorarioFyF formhorariofyf = new FormHorarioFyF(this);
+            formhorariofyf.Show();
+            this.Hide();
+          
+        }
+
+        private void btnBW_Click(object sender, EventArgs e)
+        {
+            FormHorarioBW formhorariobw = new FormHorarioBW(this);
+            formhorariobw.Show();
+            this.Hide();
+        }
+
+        private void btnBW2_Click(object sender, EventArgs e)
+        {
+            FormHorarioBW formhorariobw = new FormHorarioBW(this);
+            formhorariobw.Show();
+            this.Hide();
+        }
+
+        private void btnSilencio_Click(object sender, EventArgs e)
+        {
+            FormHorarioSilencio formsilencio = new FormHorarioSilencio(this);
+            formsilencio.Show();
+            this.Hide();
+
+        }
+
+        private void btnSilencio2_Click(object sender, EventArgs e)
+        {
+            FormHorarioSilencio formsilencio = new FormHorarioSilencio(this);
+            formsilencio.Show();
+            this.Hide();
+        }
+    }
+}
