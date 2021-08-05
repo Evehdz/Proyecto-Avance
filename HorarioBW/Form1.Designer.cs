d3cfb2679fce8de1d5b725622b9cdc7dc58641e6
Evehdz <88472990+Evehdz@users.noreply.github.com>
2021-08-05 02:07:12 -0500

    Add files via upload

---
+++ HorarioBW/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace HorarioBW
+{
+    partial class FormHorarioBW
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHorarioBW));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnBWh1 = new System.Windows.Forms.Button();
+            this.btnBWh2 = new System.Windows.Forms.Button();
+            this.btnBWh3 = new System.Windows.Forms.Button();
+            this.btnregresar2 = new System.Windows.Forms.Button();
+            this.lbNombreBW = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(41, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(115, 135);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(194, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SINOPSIS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(194, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(429, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(194, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "PG-13";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(259, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "134 min";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Location = new System.Drawing.Point(116, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 16);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "ESP";
+            // 
+            // btnBWh1
+            // 
+            this.btnBWh1.Location = new System.Drawing.Point(197, 192);
+            this.btnBWh1.Name = "btnBWh1";
+            this.btnBWh1.Size = new System.Drawing.Size(55, 23);
+            this.btnBWh1.TabIndex = 2;
+            this.btnBWh1.Text = "13:30";
+            this.btnBWh1.UseVisualStyleBackColor = true;
+            this.btnBWh1.Click += new System.EventHandler(this.btnBWh1_Click);
+            // 
+            // btnBWh2
+            // 
+            this.btnBWh2.Location = new System.Drawing.Point(305, 192);
+            this.btnBWh2.Name = "btnBWh2";
+            this.btnBWh2.Size = new System.Drawing.Size(55, 23);
+            this.btnBWh2.TabIndex = 2;
+            this.btnBWh2.Text = "15:30";
+            this.btnBWh2.UseVisualStyleBackColor = true;
+            this.btnBWh2.Click += new System.EventHandler(this.btnBWh2_Click);
+            // 
+            // btnBWh3
+            // 
+            this.btnBWh3.Location = new System.Drawing.Point(413, 192);
+            this.btnBWh3.Name = "btnBWh3";
+            this.btnBWh3.Size = new System.Drawing.Size(55, 23);
+            this.btnBWh3.TabIndex = 2;
+            this.btnBWh3.Text = "17:30";
+            this.btnBWh3.UseVisualStyleBackColor = true;
+            this.btnBWh3.Click += new System.EventHandler(this.btnBWh3_Click);
+            // 
+            // btnregresar2
+            // 
+            this.btnregresar2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnregresar2.BackgroundImage")));
+            this.btnregresar2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnregresar2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnregresar2.Location = new System.Drawing.Point(41, 261);
+            this.btnregresar2.Name = "btnregresar2";
+            this.btnregresar2.Size = new System.Drawing.Size(35, 20);
+            this.btnregresar2.TabIndex = 3;
+            this.btnregresar2.UseVisualStyleBackColor = true;
+            this.btnregresar2.Click += new System.EventHandler(this.btnregresar2_Click);
+            // 
+            // lbNombreBW
+            // 
+            this.lbNombreBW.AutoSize = true;
+            this.lbNombreBW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNombreBW.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbNombreBW.Location = new System.Drawing.Point(41, 21);
+            this.lbNombreBW.Name = "lbNombreBW";
+            this.lbNombreBW.Size = new System.Drawing.Size(110, 20);
+            this.lbNombreBW.TabIndex = 4;
+            this.lbNombreBW.Text = "Black Widow";
+            // 
+            // FormHorarioBW
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbNombreBW);
+            this.Controls.Add(this.btnregresar2);
+            this.Controls.Add(this.btnBWh3);
+            this.Controls.Add(this.btnBWh2);
+            this.Controls.Add(this.btnBWh1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormHorarioBW";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnBWh1;
+        private System.Windows.Forms.Button btnBWh2;
+        private System.Windows.Forms.Button btnBWh3;
+        private System.Windows.Forms.Button btnregresar2;
+        private System.Windows.Forms.Label lbNombreBW;
+    }
+}
+
---
+++ HorarioBW/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using SelecciondeBoletos;
+using System.Windows.Forms;
+
+namespace HorarioBW
+{
+    public partial class FormHorarioBW : Form
+    {
+        private Form Formpa;
+        //Asignamos la variable form al form que contiene el horario de la pelicula
+
+        public FormHorarioBW(Form Formpa)
+        {
+            InitializeComponent();
+            this.Formpa = Formpa;
+        }
+        //El boton regresar tiene como instruccion cerrar el form con los horarios y regresar a la cartelera
+        private void btnregresar2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Formpa.Show();
+        }
+        //Los tres botones siguientes se utilizan cuando el usuario da click en un horario y este abre la ventana de seleccion de boletos
+        private void btnBWh1_Click(object sender, EventArgs e)
+        {
+            FormSeleccionBoletos formseleccion = new FormSeleccionBoletos();
+            formseleccion.Show();
+            this.Hide();
+        }
+
+        private void btnBWh2_Click(object sender, EventArgs e)
+        {
+            FormSeleccionBoletos formseleccion = new FormSeleccionBoletos();
+            formseleccion.Show();
+            this.Hide();
+        }
+
+        private void btnBWh3_Click(object sender, EventArgs e)
+        {
+            FormSeleccionBoletos formseleccion = new FormSeleccionBoletos();
+            formseleccion.Show();
+            this.Hide();
+        }
+    }
+}
